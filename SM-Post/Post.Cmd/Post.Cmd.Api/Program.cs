4cf49bf470816367355acc6ed593744b91d935e7
Admin <Admin@LAPTOP-4P4HLUTQ>
2022-09-12 18:24:17 +0700

    Add aggregate, EventModel, EventStore, EventStoreRepository

--- SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,6 +1,19 @@
+using CQRS.Core.Domain;
+using CQRS.Core.Infrastructure;
+using Post.Cmd.Domain.Aggregates;
+using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
+//builder.Services.Configure<ProducerConfig>(builder.Configuration.GetSection(nameof(ProducerConfig)));
+builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
+//builder.Services.AddScoped<IEventProducer, EventProducer>();
+//builder.Services.AddScoped<IEventStore, EventStore>();
+//builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
+//builder.Services.AddScoped<ICommandHandler, CommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
