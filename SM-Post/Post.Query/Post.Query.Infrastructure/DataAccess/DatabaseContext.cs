3af67dd2408df6579256aef231fd23639f9fc959
Admin <Admin@LAPTOP-4P4HLUTQ>
2022-09-15 00:08:51 +0700

    Add Post API

--- SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContext.cs
+++ SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContext.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Post.Query.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Post.Query.Infrastructure.DataAccess
 {
--- SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContextFactory.cs
+++ SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContextFactory.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Post.Query.Infrastructure.DataAccess
 {
@@ -11,7 +6,7 @@ namespace Post.Query.Infrastructure.DataAccess
     {
         private readonly Action<DbContextOptionsBuilder> _configureDbContext;
 
-        public DatabaseContextFactory(Action<DbContextOptionsBuilder> configureDbContext)
+        public DatabaseContextFactory(Action<DbContextOptionsBuilder> configureDbContext) 
         {
             _configureDbContext = configureDbContext;
         }
